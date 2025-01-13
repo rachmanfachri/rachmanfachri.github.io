02b5f77d9bf8655ea4b8010c6188a41b4904f9b0
rachmanfachri <laukbasi@gmail.com>
2025-01-13 21:37:35 +0700

    change naming

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Fachri Rachmanda's Portfolio",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
