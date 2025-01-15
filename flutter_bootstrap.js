c28220c717d07e9c9219e0b9c2930151e3adc829
rachmanfachri <laukbasi@gmail.com>
2025-01-15 22:27:37 +0700

    new web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3117862647"
+    serviceWorkerVersion: "3928327555"
   }
 });
