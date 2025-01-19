e1489a739695b7330ac9cdd3c69beb80f50c203d
rachmanfachri <laukbasi@gmail.com>
2025-01-19 17:45:18 +0700

    change project content as card

--- lib/app/modules/projects/views/projects_view.dart
+++ lib/app/modules/projects/views/projects_view.dart
@@ -69,44 +69,43 @@ class ProjectsView extends GetView<ProjectsController> {
                           style: GoogleFonts.poppins(
                               color: Colors.white, fontSize: 30),
                         ),
-                        SizedBox(height: 30),
+                        const SizedBox(height: 10),
                         Text(
-                          "ACA Pocket (digital marketing app)",
+                          "hover the card to read more",
+                          textAlign: TextAlign.center,
                           style: GoogleFonts.poppins(
-                              color: Colors.white, fontSize: 22),
+                              color: Colors.white, fontSize: 14),
                         ),
-                        SizedBox(height: 20),
-                        Image.asset('assets/images/aca.jpg', width: 300),
-                        SizedBox(height: 20),
-                        Text(
-                          "my very first professional project as a fresh"
-                          " graduate. this app can only be used internally"
-                          " by ACA employee. the app covers essential workflow of"
-                          " insurance marketing officer such as reporting, handling"
-                          " insurance cover letter, asset and applicant"
-                          " survey etc. available ony in play store.",
-                          style: GoogleFonts.poppins(
-                              color: Colors.white, fontSize: 16),
-                        ),
-                        SizedBox(height: 30),
-                        Text(
-                          "unreleased, in-progress job portal app",
-                          style: GoogleFonts.poppins(
-                              color: Colors.white, fontSize: 22),
-                        ),
-                        SizedBox(height: 20),
-                        Image.asset('assets/images/rkt.png', width: 300),
-                        SizedBox(height: 20),
-                        Text(
-                          "due to non-disclosure agreement, i can't giving much"
-                          " about this app aside from what it is and its basic"
-                          " functionality. it's a job portal app which is part"
-                          " of a bigger ecosystem of a jobseeking service. the"
-                          " app itself let the user to register, fill up their"
-                          " information for jobseeking purposes, and search and"
-                          " apply for the job that available.",
-                          style: GoogleFonts.poppins(
-                              color: Colors.white, fontSize: 16),
+                        const SizedBox(height: 30),
+                        Wrap(
+                          direction: Axis.horizontal,
+                          children: [
+                            projectCard(
+                              title: "ACA Pocket\n(digital marketing app)",
+                              imageSrc: 'assets/images/aca.jpg',
+                              description:
+                                  "my very first professional project as a fresh"
+                                  " graduate. this app can only be used internally"
+                                  " by ACA employee. the app covers essential workflow of"
+                                  " insurance marketing officer such as reporting, handling"
+                                  " insurance cover letter, asset and applicant"
+                                  " survey etc. available ony in play store.",
+                              maxLines: 6,
+                            ),
+                            projectCard(
+                              title: "unreleased, in-progress job portal app",
+                              imageSrc: 'assets/images/rkt.png',
+                              description:
+                                  "due to non-disclosure agreement, i can't giving much"
+                                  " about this app aside from what it is and its basic"
+                                  " functionality. it's a job portal app which is part"
+                                  " of a bigger ecosystem of a jobseeking service. the"
+                                  " app itself let the user to register, fill up their"
+                                  " information for jobseeking purposes, and search and"
+                                  " apply for the job that available.",
+                              maxLines: 4,
+                            ),
+                          ],
                         ),
                       ],
                     ),
@@ -117,6 +116,52 @@ class ProjectsView extends GetView<ProjectsController> {
           ),
           siteAppBar('projects'),
         ],
+      ),
+    );
+  }
+
+  Widget projectCard({
+    required String title,
+    required String imageSrc,
+    required String description,
+    required int maxLines,
+  }) {
+    final onHover = false.obs;
+
+    return Obx(
+      () => Card(
+        // margin: const EdgeInsets.all(20),
+        borderOnForeground: true,
+        color: Colors.transparent,
+        child: TextButton(
+          onPressed: () {},
+          onHover: (value) => onHover.value = value,
+          style: TextButton.styleFrom(overlayColor: Colors.transparent),
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            constraints: BoxConstraints(
+                maxWidth: 360,
+                maxHeight: onHover.value ? double.infinity : 540),
+            child: Column(
+              children: [
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 22),
+                ),
+                SizedBox(height: 20),
+                Image.asset(imageSrc, width: 300),
+                SizedBox(height: 20),
+                Text(
+                  description,
+                  maxLines: onHover.value ? null : maxLines,
+                  overflow: onHover.value ? null : TextOverflow.ellipsis,
+                  style: GoogleFonts.poppins(color: Colors.white, fontSize: 16),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
